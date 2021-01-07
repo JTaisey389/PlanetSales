816e36d9c6a296d3d62b60177b956a9b0032b9e5
Jason Taisey <jdtaisey89@icloud.com>
2021-01-06 16:44:51 -0800

    Content to website

---
+++ app2.js
@@ -0,0 +1,8 @@
+if (userName == 'Jason') {
+    document.write('<h3>' + 'Welcome Squirrel' + '<h/3>');
+} else {
+    document.write('<h3>' + 'Welcome Squirrel' + '<h/3>')
+}
+if (userName == 'Jason') {
+	document.write('<h3>' + 'Welcome Guest' + '<h/3>');
+} 
